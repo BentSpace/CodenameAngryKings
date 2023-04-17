0f4e1afa9568f05810849ba585065f6be3388d7c
Nathan Bertram <bentspacetime@gmail.com>
2023-04-17 15:26:16 -0700

    Catapult fires

--- Assets/Scripts/FireCatapult.cs
+++ Assets/Scripts/FireCatapult.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class FireCatapult : MonoBehaviour
 {
-    public GameObject hinge;
+    // public GameObject hinge;
+    // public float fireSpeed = 90;
+    Rigidbody catapultRigidbody;
+    public float catapultForce = 1000000f;
 
-    public float fireSpeed = 90;
     // Start is called before the first frame update
     void Start()
     {
-
+        catapultRigidbody = GetComponent<Rigidbody>();
+        Fire();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.RotateAround(hinge.transform.position, Vector3.forward, fireSpeed * Time.deltaTime);
+        catapultRigidbody.AddForce(transform.up * catapultForce);
+        // transform.RotateAround(hinge.transform.position, Vector3.forward, fireSpeed * Time.deltaTime);
     }
-    
+
+    void Fire()
+    {
+        
+    }
 }
