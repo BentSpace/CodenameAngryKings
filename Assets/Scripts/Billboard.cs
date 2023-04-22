6ececa21f67c975007786236ab63ebe826dc3c6c
smasterson64 <smasterson64@gmail.com>
2023-04-21 23:08:32 -0700

    Minor changes
    Added texture to ground, added game states to game manager

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.LookAt(target);
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,16 @@ public class GameManager : MonoBehaviour
     public FireCatapult Player1;
     public FireCatapult Player2;
 
+
+    enum gameState {PAUSED = -1, GAMESTART, PLAYMENU, ATTACK, AIMING, BUILD, GAMEEND};  // game states:
+                                                                                        // PAUSED: pause menu loaded, can reset game, go back to main menu
+                                                                                        // GAMESTART: Start of the game, decide who goes first
+                                                                                        // PLAYMENU: Menu where current player selects whether to build or attack
+                                                                                        // ATTACK: state where current player can select weapon to fire
+                                                                                        // AIMING: state where current player can adjust selected weapon
+                                                                                        // BUILD: state where current player can place bricks or weapons
+                                                                                        // GAMEEND: results of match are displayed, and a new game can begin
+    gameState state = gameState.GAMESTART;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +38,24 @@ public class GameManager : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             endTurn();
+        }
+
+        switch (state)
+        {
+            case gameState.PAUSED:
+                break;
+            case gameState.GAMESTART:
+                break;
+            case gameState.PLAYMENU:
+                break;
+            case gameState.ATTACK:
+                break;
+            case gameState.AIMING:
+                break;
+            case gameState.BUILD:
+                break;
+            case gameState.GAMEEND:
+                break;
         }
     }
 
