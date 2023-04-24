be4584edf0a57bdec2cf4cfadcb3d0d8e6a3552f
Nathan Bertram <bentspacetime@gmail.com>
2023-04-24 14:32:00 -0700

    Catapult Reload and side to side aim

---
+++ Assets/Scripts/AimCatapult.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCatapult : MonoBehaviour
+{
+    public float rotationSpeed = 30f; // Degrees per second
+    public float rotationAmount = 5f; // Degrees of rotation per key press
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            StartCoroutine(RotateCatapult(-1)); // Rotate left
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            StartCoroutine(RotateCatapult(1)); // Rotate right
+        }
+    }
+
+    IEnumerator RotateCatapult(int direction)
+    {
+        float rotationRemaining = rotationAmount;
+
+        while (rotationRemaining > 0)
+        {
+            float rotationThisFrame = Mathf.Min(rotationSpeed * Time.deltaTime, rotationRemaining);
+            transform.Rotate(Vector3.up, direction * rotationThisFrame, Space.World);
+            rotationRemaining -= rotationThisFrame;
+            yield return null;
+        }
+    }
+}
+
+
